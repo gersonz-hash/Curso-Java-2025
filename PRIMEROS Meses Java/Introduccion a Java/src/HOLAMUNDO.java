f3c35c8177df6205b2b6532a0f81239aee2e0caf
Gerson Zeceña <gerdavmontufar@gmail.com>
2026-04-03 21:52:17 -0600

    Eliminando estructura incorrecta

--- PRIMEROS Meses Java/Introduccion a Java/src/HOLAMUNDO.java
+++
@@ -1,7 +0,0 @@
-public class HOLAMUNDO {
-    public static void main(String[] args) {
-        System.out.println("HOLA MUNDO ");
-        System.out.println("SALUDOS A TODOS");
-        System.out.println("Bienvenidos");
-    }
-}
